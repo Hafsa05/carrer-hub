ec9a55668eaf366c41d2d53c4aed7ca3ab27ff21
Hafsa05 <hafsa@g.bracu.ac.bd>
2023-04-12 23:49:10 +0600

    graph added to the graph page

--- src/components/Graph/Graph.jsx
+++ src/components/Graph/Graph.jsx
@@ -1,67 +1,67 @@
-import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+// import React from 'react';
+// import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 
-const Graph = () => {
-    const myMarks=[
-        {
-            id:1,
-            name: "Assignment1",
-            marks:57
-        },
-        {
-            id:2,
-            name: "Assignment2",
-            marks:58
-        },
-        {
-            id:3,
-            name: "Assignment3",
-            marks:57
-        },
-        {
-            id:4,
-            name: "Assignment4",
-            marks:49
-        },
-        {
-            id:5,
-            name: "Assignment5",
-            marks:58
-        },
-        {
-            id:6,
-            name: "Assignment6",
-            marks:58
-        },
-        {
-            id:7,
-            name: "Assignment7",
-            marks:60
-        },
-        {
-            id:8,
-            name: "Assignment8",
-            marks:27
-        },
-    ];
-    return (
-        <div>
-            <h2 className='font-bold text-4xl mb-36 mt-16'>Graph Page</h2>
-            {/* <ResponsiveContainer width="100%" height="100%"> */}
-            <LineChart
-                width={1200}
-                height={400}
-                data={myMarks}
-            >
-                <XAxis dataKey={"name"}></XAxis>
-                <YAxis dataKey={"marks"}></YAxis>
-                <Line type="monotone" dataKey="marks" stroke="#8884d8" strokeDasharray="5 5" />
+// const Graph = () => {
+//     const myMarks=[
+//         {
+//             id:1,
+//             name: "Assignment1",
+//             marks:57
+//         },
+//         {
+//             id:2,
+//             name: "Assignment2",
+//             marks:58
+//         },
+//         {
+//             id:3,
+//             name: "Assignment3",
+//             marks:57
+//         },
+//         {
+//             id:4,
+//             name: "Assignment4",
+//             marks:49
+//         },
+//         {
+//             id:5,
+//             name: "Assignment5",
+//             marks:58
+//         },
+//         {
+//             id:6,
+//             name: "Assignment6",
+//             marks:58
+//         },
+//         {
+//             id:7,
+//             name: "Assignment7",
+//             marks:60
+//         },
+//         {
+//             id:8,
+//             name: "Assignment8",
+//             marks:27
+//         },
+//     ];
+//     return (
+//         <div>
+//             <h2 className='font-bold text-4xl mb-36 mt-16'>Graph Page</h2>
+//             {/* <ResponsiveContainer width="100%" height="100%"> */}
+//             <LineChart
+//                 width={1200}
+//                 height={400}
+//                 data={myMarks}
+//             >
+//                 <XAxis dataKey={"name"}></XAxis>
+//                 <YAxis dataKey={"marks"}></YAxis>
+//                 <Line type="monotone" dataKey="marks" stroke="#8884d8" strokeDasharray="5 5" />
           
-            </LineChart>
-            {/* </ResponsiveContainer> */}
-        </div>
-    );
-};
+//             </LineChart>
+//             {/* </ResponsiveContainer> */}
+//         </div>
+//     );
+// };
 
-export default Graph;
+// export default Graph;
